bbd252387c1b27841d913ad7621db0411d3af40f
Jacob <jakegibs617@gmail.com>
2016-01-19 21:51:23 -0500

    init commit

--- starwarsfighters/ViewController.swift
+++ starwarsfighters/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bluePic: UIImageView!
+    @IBOutlet weak var redPic: UIImageView!
+    
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func blueClicked(sender: AnyObject) {
+        redPic.hidden = true
+        bluePic.hidden = false
+    }
 
+    @IBAction func redClicked(sender: AnyObject) {
+        bluePic.hidden = true
+        redPic.hidden = false
+    }
+
+    @IBAction func resetButton(sender: AnyObject) {
+        bluePic.hidden = false
+        redPic.hidden = false
+    }
 }
 
